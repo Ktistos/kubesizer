8e0cca6e51ef2901ce6ca480fe62782deaf8de68
Ktistos <ktistkostas@gmail.com>
2026-07-03 19:40:26 +0300

    added logic to initialize the system with services(nodes) and then configure the adjacency list with the edges of the services

--- performance_models/service.py
+++ performance_models/service.py
@@ -2,13 +2,35 @@ from performance_models.edge import Edge
 
 
 class Service():
-    def __init__(self, name, adjacency_list={},gamma = 0, mu=0, replicas=1):
+    def __init__(self, parent_system, name,gamma = 0, mu=0, replicas=1):
+        
+        self.__parent_system = parent_system
         self.__name = name
-        self.__adjacency_list = adjacency_list
+        self.__adjacency_list = []
         self.__gamma = gamma
         self.__mu = mu   
         self.__replicas = replicas
 
+    def register_edges(self, edges_spec):
+        total_probabiliy = 0
+
+        for edge in edges_spec:
+            probability = edges_spec[edge]
+            total_probabiliy += probability
+            edge_component = Edge(self,self.get_parent_system().get_service(edge) ,probability)
+            self.add_edge(edge_component)
+        
+        #this assumes that the remaining routing probability points to the exit of the system
+        remaining_probability = 1 - total_probabiliy
+        if remaining_probability > 0:
+            edge_component = Edge(self, None, remaining_probability)
+            self.add_edge(edge_component)
+        elif total_probabiliy > 1:
+            raise ValueError(f"Total probability of edges for service {self.get_name()} exceeds 1.")
+
+
+    def get_parent_system(self):
+        return self.__parent_system
 
     def get_name(self):
         return self.__name
@@ -26,7 +48,7 @@ class Service():
         return self.__replicas
     
     def add_edge(self, edge : Edge):
-        self.__adjacency_list.append(edge)
+        self.get_adjacency_list().append(edge)
 
 class ServiceGraph():
     def __init__(self, services):
--- performance_models/system.py
+++ performance_models/system.py
@@ -11,19 +11,22 @@ class QueueingNetworkType(str, Enum):
 
 class System():
 
-
-    
     def __init__(self, name,service_spec, type : QueueingNetworkType):
         self.__name = name
         #type refers to either open or closed system
         self.__type = type
-        self.__services = []
+        self.__services = {}
 
         for service in service_spec:
 
-            service_component = Service(service,service_spec[service]['routes'], service_spec[service]['gamma'], service_spec[service]['mu'], service_spec[service]['replicas'])
+            service_component = Service(self, service, service_spec[service]['gamma'], service_spec[service]['mu'], service_spec[service]['replicas'])
 
-            self.add_service(service_component)
+            self.add_service(service,service_component)
+
+        for _, service_component in self.get_services().items():
+            service_component.register_edges(service_spec[service]['routes'])
+
+
 
 
     def get_name(self):
@@ -32,9 +35,15 @@ class System():
     def get_type(self):
         return self.__type
 
-    def add_service(self, component):
-        self.__services.append(component)
+    def add_service(self, name, component):
+        self.__services[name] = component
+
+    def get_service(self, name):
+        return self.__services.get(name, None)
     
+    def get_services(self):
+        return self.__services
+
     def solve(self):
         # Placeholder for solving the system
         pass
